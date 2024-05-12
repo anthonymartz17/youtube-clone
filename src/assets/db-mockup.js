c7894a3776c53cf626b2f58c785352e91cbfdcb3
Antonio Martinez <antonio.fr.martinezc@hotmail.com>
2024-05-12 12:54:26 -0400

    created videoList component , added css modules to style videos previews.added the videos preview component

--- src/assets/db-mockup.js
+++ src/assets/db-mockup.js
@@ -1,4 +1,4 @@
-const db_mockup = {
+export const db_mockup = {
   "kind": "youtube#videoListResponse",
   "etag": "_i_yDa7sCheOEjcitYwfZ5BgITM",
   "items": [
@@ -53,7 +53,423 @@ const db_mockup = {
               "defaultAudioLanguage": "en"
           }
       },
-      // Other video items...
+    
+      {
+          "kind": "youtube#video",
+          "etag": "EJMZrf-DxfgzU89JSvgJaPPGGeI",
+          "id": "4QIZE708gJ4",
+          "snippet": {
+              "publishedAt": "2024-05-10T14:00:06Z",
+              "channelId": "UCOhtMAg7xh8wv_wUHMgFc-Q",
+              "title": "Post Malone - I Had Some Help (feat. Morgan Wallen) (Official Video)",
+              "description": "Official music video for 'I Had Some Help'...",
+              "thumbnails": {
+                  "default": {
+                      "url": "https://i.ytimg.com/vi/4QIZE708gJ4/default.jpg",
+                      "width": 120,
+                      "height": 90
+                  },
+                  "medium": {
+                      "url": "https://i.ytimg.com/vi/4QIZE708gJ4/mqdefault.jpg",
+                      "width": 320,
+                      "height": 180
+                  },
+                  "high": {
+                      "url": "https://i.ytimg.com/vi/4QIZE708gJ4/hqdefault.jpg",
+                      "width": 480,
+                      "height": 360
+                  },
+                  "standard": {
+                      "url": "https://i.ytimg.com/vi/4QIZE708gJ4/sddefault.jpg",
+                      "width": 640,
+                      "height": 480
+                  },
+                  "maxres": {
+                      "url": "https://i.ytimg.com/vi/4QIZE708gJ4/maxresdefault.jpg",
+                      "width": 1280,
+                      "height": 720
+                  }
+              },
+              "channelTitle": "PostMaloneVEVO",
+              "tags": [
+                  "post malone",
+                  "had some help",
+                  // Other tags...
+              ],
+              "categoryId": "10",
+              "liveBroadcastContent": "none",
+              "localized": {
+                  "title": "Post Malone - I Had Some Help (feat. Morgan Wallen) (Official Video)",
+                  "description": "Official music video for 'I Had Some Help'..."
+              },
+              "defaultAudioLanguage": "en"
+          }
+      },
+    
+      {
+          "kind": "youtube#video",
+          "etag": "EJMZrf-DxfgzU89JSvgJaPPGGeI",
+          "id": "4QIZE708gJ4",
+          "snippet": {
+              "publishedAt": "2024-05-10T14:00:06Z",
+              "channelId": "UCOhtMAg7xh8wv_wUHMgFc-Q",
+              "title": "Post Malone - I Had Some Help (feat. Morgan Wallen) (Official Video)",
+              "description": "Official music video for 'I Had Some Help'...",
+              "thumbnails": {
+                  "default": {
+                      "url": "https://i.ytimg.com/vi/4QIZE708gJ4/default.jpg",
+                      "width": 120,
+                      "height": 90
+                  },
+                  "medium": {
+                      "url": "https://i.ytimg.com/vi/4QIZE708gJ4/mqdefault.jpg",
+                      "width": 320,
+                      "height": 180
+                  },
+                  "high": {
+                      "url": "https://i.ytimg.com/vi/4QIZE708gJ4/hqdefault.jpg",
+                      "width": 480,
+                      "height": 360
+                  },
+                  "standard": {
+                      "url": "https://i.ytimg.com/vi/4QIZE708gJ4/sddefault.jpg",
+                      "width": 640,
+                      "height": 480
+                  },
+                  "maxres": {
+                      "url": "https://i.ytimg.com/vi/4QIZE708gJ4/maxresdefault.jpg",
+                      "width": 1280,
+                      "height": 720
+                  }
+              },
+              "channelTitle": "PostMaloneVEVO",
+              "tags": [
+                  "post malone",
+                  "had some help",
+                  // Other tags...
+              ],
+              "categoryId": "10",
+              "liveBroadcastContent": "none",
+              "localized": {
+                  "title": "Post Malone - I Had Some Help (feat. Morgan Wallen) (Official Video)",
+                  "description": "Official music video for 'I Had Some Help'..."
+              },
+              "defaultAudioLanguage": "en"
+          }
+      },
+    
+      {
+          "kind": "youtube#video",
+          "etag": "EJMZrf-DxfgzU89JSvgJaPPGGeI",
+          "id": "4QIZE708gJ4",
+          "snippet": {
+              "publishedAt": "2024-05-10T14:00:06Z",
+              "channelId": "UCOhtMAg7xh8wv_wUHMgFc-Q",
+              "title": "Post Malone - I Had Some Help (feat. Morgan Wallen) (Official Video)",
+              "description": "Official music video for 'I Had Some Help'...",
+              "thumbnails": {
+                  "default": {
+                      "url": "https://i.ytimg.com/vi/4QIZE708gJ4/default.jpg",
+                      "width": 120,
+                      "height": 90
+                  },
+                  "medium": {
+                      "url": "https://i.ytimg.com/vi/4QIZE708gJ4/mqdefault.jpg",
+                      "width": 320,
+                      "height": 180
+                  },
+                  "high": {
+                      "url": "https://i.ytimg.com/vi/4QIZE708gJ4/hqdefault.jpg",
+                      "width": 480,
+                      "height": 360
+                  },
+                  "standard": {
+                      "url": "https://i.ytimg.com/vi/4QIZE708gJ4/sddefault.jpg",
+                      "width": 640,
+                      "height": 480
+                  },
+                  "maxres": {
+                      "url": "https://i.ytimg.com/vi/4QIZE708gJ4/maxresdefault.jpg",
+                      "width": 1280,
+                      "height": 720
+                  }
+              },
+              "channelTitle": "PostMaloneVEVO",
+              "tags": [
+                  "post malone",
+                  "had some help",
+                  // Other tags...
+              ],
+              "categoryId": "10",
+              "liveBroadcastContent": "none",
+              "localized": {
+                  "title": "Post Malone - I Had Some Help (feat. Morgan Wallen) (Official Video)",
+                  "description": "Official music video for 'I Had Some Help'..."
+              },
+              "defaultAudioLanguage": "en"
+          }
+      },
+    
+      {
+          "kind": "youtube#video",
+          "etag": "EJMZrf-DxfgzU89JSvgJaPPGGeI",
+          "id": "4QIZE708gJ4",
+          "snippet": {
+              "publishedAt": "2024-05-10T14:00:06Z",
+              "channelId": "UCOhtMAg7xh8wv_wUHMgFc-Q",
+              "title": "Post Malone - I Had Some Help (feat. Morgan Wallen) (Official Video)",
+              "description": "Official music video for 'I Had Some Help'...",
+              "thumbnails": {
+                  "default": {
+                      "url": "https://i.ytimg.com/vi/4QIZE708gJ4/default.jpg",
+                      "width": 120,
+                      "height": 90
+                  },
+                  "medium": {
+                      "url": "https://i.ytimg.com/vi/4QIZE708gJ4/mqdefault.jpg",
+                      "width": 320,
+                      "height": 180
+                  },
+                  "high": {
+                      "url": "https://i.ytimg.com/vi/4QIZE708gJ4/hqdefault.jpg",
+                      "width": 480,
+                      "height": 360
+                  },
+                  "standard": {
+                      "url": "https://i.ytimg.com/vi/4QIZE708gJ4/sddefault.jpg",
+                      "width": 640,
+                      "height": 480
+                  },
+                  "maxres": {
+                      "url": "https://i.ytimg.com/vi/4QIZE708gJ4/maxresdefault.jpg",
+                      "width": 1280,
+                      "height": 720
+                  }
+              },
+              "channelTitle": "PostMaloneVEVO",
+              "tags": [
+                  "post malone",
+                  "had some help",
+                  // Other tags...
+              ],
+              "categoryId": "10",
+              "liveBroadcastContent": "none",
+              "localized": {
+                  "title": "Post Malone - I Had Some Help (feat. Morgan Wallen) (Official Video)",
+                  "description": "Official music video for 'I Had Some Help'..."
+              },
+              "defaultAudioLanguage": "en"
+          }
+      },
+    
+      {
+          "kind": "youtube#video",
+          "etag": "EJMZrf-DxfgzU89JSvgJaPPGGeI",
+          "id": "4QIZE708gJ4",
+          "snippet": {
+              "publishedAt": "2024-05-10T14:00:06Z",
+              "channelId": "UCOhtMAg7xh8wv_wUHMgFc-Q",
+              "title": "Post Malone - I Had Some Help (feat. Morgan Wallen) (Official Video)",
+              "description": "Official music video for 'I Had Some Help'...",
+              "thumbnails": {
+                  "default": {
+                      "url": "https://i.ytimg.com/vi/4QIZE708gJ4/default.jpg",
+                      "width": 120,
+                      "height": 90
+                  },
+                  "medium": {
+                      "url": "https://i.ytimg.com/vi/4QIZE708gJ4/mqdefault.jpg",
+                      "width": 320,
+                      "height": 180
+                  },
+                  "high": {
+                      "url": "https://i.ytimg.com/vi/4QIZE708gJ4/hqdefault.jpg",
+                      "width": 480,
+                      "height": 360
+                  },
+                  "standard": {
+                      "url": "https://i.ytimg.com/vi/4QIZE708gJ4/sddefault.jpg",
+                      "width": 640,
+                      "height": 480
+                  },
+                  "maxres": {
+                      "url": "https://i.ytimg.com/vi/4QIZE708gJ4/maxresdefault.jpg",
+                      "width": 1280,
+                      "height": 720
+                  }
+              },
+              "channelTitle": "PostMaloneVEVO",
+              "tags": [
+                  "post malone",
+                  "had some help",
+                  // Other tags...
+              ],
+              "categoryId": "10",
+              "liveBroadcastContent": "none",
+              "localized": {
+                  "title": "Post Malone - I Had Some Help (feat. Morgan Wallen) (Official Video)",
+                  "description": "Official music video for 'I Had Some Help'..."
+              },
+              "defaultAudioLanguage": "en"
+          }
+      },
+    
+      {
+          "kind": "youtube#video",
+          "etag": "EJMZrf-DxfgzU89JSvgJaPPGGeI",
+          "id": "4QIZE708gJ4",
+          "snippet": {
+              "publishedAt": "2024-05-10T14:00:06Z",
+              "channelId": "UCOhtMAg7xh8wv_wUHMgFc-Q",
+              "title": "Post Malone - I Had Some Help (feat. Morgan Wallen) (Official Video)",
+              "description": "Official music video for 'I Had Some Help'...",
+              "thumbnails": {
+                  "default": {
+                      "url": "https://i.ytimg.com/vi/4QIZE708gJ4/default.jpg",
+                      "width": 120,
+                      "height": 90
+                  },
+                  "medium": {
+                      "url": "https://i.ytimg.com/vi/4QIZE708gJ4/mqdefault.jpg",
+                      "width": 320,
+                      "height": 180
+                  },
+                  "high": {
+                      "url": "https://i.ytimg.com/vi/4QIZE708gJ4/hqdefault.jpg",
+                      "width": 480,
+                      "height": 360
+                  },
+                  "standard": {
+                      "url": "https://i.ytimg.com/vi/4QIZE708gJ4/sddefault.jpg",
+                      "width": 640,
+                      "height": 480
+                  },
+                  "maxres": {
+                      "url": "https://i.ytimg.com/vi/4QIZE708gJ4/maxresdefault.jpg",
+                      "width": 1280,
+                      "height": 720
+                  }
+              },
+              "channelTitle": "PostMaloneVEVO",
+              "tags": [
+                  "post malone",
+                  "had some help",
+                  // Other tags...
+              ],
+              "categoryId": "10",
+              "liveBroadcastContent": "none",
+              "localized": {
+                  "title": "Post Malone - I Had Some Help (feat. Morgan Wallen) (Official Video)",
+                  "description": "Official music video for 'I Had Some Help'..."
+              },
+              "defaultAudioLanguage": "en"
+          }
+      },
+    
+      {
+          "kind": "youtube#video",
+          "etag": "EJMZrf-DxfgzU89JSvgJaPPGGeI",
+          "id": "4QIZE708gJ4",
+          "snippet": {
+              "publishedAt": "2024-05-10T14:00:06Z",
+              "channelId": "UCOhtMAg7xh8wv_wUHMgFc-Q",
+              "title": "Post Malone - I Had Some Help (feat. Morgan Wallen) (Official Video)",
+              "description": "Official music video for 'I Had Some Help'...",
+              "thumbnails": {
+                  "default": {
+                      "url": "https://i.ytimg.com/vi/4QIZE708gJ4/default.jpg",
+                      "width": 120,
+                      "height": 90
+                  },
+                  "medium": {
+                      "url": "https://i.ytimg.com/vi/4QIZE708gJ4/mqdefault.jpg",
+                      "width": 320,
+                      "height": 180
+                  },
+                  "high": {
+                      "url": "https://i.ytimg.com/vi/4QIZE708gJ4/hqdefault.jpg",
+                      "width": 480,
+                      "height": 360
+                  },
+                  "standard": {
+                      "url": "https://i.ytimg.com/vi/4QIZE708gJ4/sddefault.jpg",
+                      "width": 640,
+                      "height": 480
+                  },
+                  "maxres": {
+                      "url": "https://i.ytimg.com/vi/4QIZE708gJ4/maxresdefault.jpg",
+                      "width": 1280,
+                      "height": 720
+                  }
+              },
+              "channelTitle": "PostMaloneVEVO",
+              "tags": [
+                  "post malone",
+                  "had some help",
+                  // Other tags...
+              ],
+              "categoryId": "10",
+              "liveBroadcastContent": "none",
+              "localized": {
+                  "title": "Post Malone - I Had Some Help (feat. Morgan Wallen) (Official Video)",
+                  "description": "Official music video for 'I Had Some Help'..."
+              },
+              "defaultAudioLanguage": "en"
+          }
+      },
+    
+      {
+          "kind": "youtube#video",
+          "etag": "EJMZrf-DxfgzU89JSvgJaPPGGeI",
+          "id": "4QIZE708gJ4",
+          "snippet": {
+              "publishedAt": "2024-05-10T14:00:06Z",
+              "channelId": "UCOhtMAg7xh8wv_wUHMgFc-Q",
+              "title": "Post Malone - I Had Some Help (feat. Morgan Wallen) (Official Video)",
+              "description": "Official music video for 'I Had Some Help'...",
+              "thumbnails": {
+                  "default": {
+                      "url": "https://i.ytimg.com/vi/4QIZE708gJ4/default.jpg",
+                      "width": 120,
+                      "height": 90
+                  },
+                  "medium": {
+                      "url": "https://i.ytimg.com/vi/4QIZE708gJ4/mqdefault.jpg",
+                      "width": 320,
+                      "height": 180
+                  },
+                  "high": {
+                      "url": "https://i.ytimg.com/vi/4QIZE708gJ4/hqdefault.jpg",
+                      "width": 480,
+                      "height": 360
+                  },
+                  "standard": {
+                      "url": "https://i.ytimg.com/vi/4QIZE708gJ4/sddefault.jpg",
+                      "width": 640,
+                      "height": 480
+                  },
+                  "maxres": {
+                      "url": "https://i.ytimg.com/vi/4QIZE708gJ4/maxresdefault.jpg",
+                      "width": 1280,
+                      "height": 720
+                  }
+              },
+              "channelTitle": "PostMaloneVEVO",
+              "tags": [
+                  "post malone",
+                  "had some help",
+                  // Other tags...
+              ],
+              "categoryId": "10",
+              "liveBroadcastContent": "none",
+              "localized": {
+                  "title": "Post Malone - I Had Some Help (feat. Morgan Wallen) (Official Video)",
+                  "description": "Official music video for 'I Had Some Help'..."
+              },
+              "defaultAudioLanguage": "en"
+          }
+      },
+    
   ],
   "nextPageToken": "CAUQAA",
   "pageInfo": {
