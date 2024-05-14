6b7ff49779d89d99d120db3f5b4f426a8c994c07
Bryan Alcantara <alcantarabryan0418@gmail.com>
2024-05-13 20:57:47 -0400

    added the functionality on the sandwich button to make the nav dissappear and come back. Added the tagbar on top without the scroll with data dummy.

--- src/componets/Navigation.jsx
+++ src/componets/Navigation.jsx
@@ -8,8 +8,8 @@ export function Navigation(){
             <ul className="ul-for-sidebar">
                 <Link to="/"><i className="fa-solid fa-house ham-menu_li_image"></i>
                 <li className="ham-menu_li">Home</li></Link>
-                <Link to="/about"><li className="ham-menu_li">About</li></Link>
-                <img className="ham-menu_li_image"src="src\Images\youtube-shorts-logo-15252.png"></img>
+                <Link to="/about"><i class="fa-solid fa-circle-info ham-menu_li_image"></i><li className="ham-menu_li">About</li></Link>
+                <img className="ham-menu_li_image" src="src\Images\youtube-shorts-logo-15252.png"></img>
                 <li className="ham-menu_li">Shorts</li>
                 <img className="ham-menu_li_image" src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/3264373/subscriptions-icon-md.png"></img>
                 <li className="ham-menu_li">Subscriptions</li>
@@ -17,12 +17,12 @@ export function Navigation(){
                 <li className="ham-menu_li">You</li>
             </ul>
         </nav>
-        <div className="off-screen-menu">
+        {/* <div className="off-screen-menu">
             <ul>
                 <li>Home</li>
                 <li>About</li>
             </ul>
-        </div>
+        </div> */}
         </>
     )
 }
---
+++ src/componets/TagBar.jsx
@@ -0,0 +1,12 @@
+import "../assets/styles/TagBar.css"
+
+export function TagBar(){
+    return(
+        <ul className="tagbar-list">
+            <li className="tagbar-list_li">All</li>
+            <li className="tagbar-list_li">APIs</li>
+            <li className="tagbar-list_li">Javascript</li>
+            <li className="tagbar-list_li">Gaming</li>
+        </ul>
+    )
+}
