b57df4002d2f22ee10f604965c931b4a99d52df7
Antonio <anthonymartz17@hotmail.com>
2024-05-14 21:08:33 -0400

    cleaned up home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,15 +3,12 @@ import { useEffect, useState } from "react";
 import VideoPreview from "../componets/VideoPreview";
 import classes from "../assets/styles/Videos.module.css";
 import { TagBar } from "../componets/TagBar";
-import { Link } from "react-router-dom";
+
 
 export default function Home({ videos }) {
-	console.log(videos)
+
 	return (
 		<div>
-			<Link to="/watch/7NOSDKb0HlU" className="bg-blue-800">
-				test
-			</Link>
 			<TagBar />
 			<div className={classes.videos}>
 				{videos.items.map((ele) => {
